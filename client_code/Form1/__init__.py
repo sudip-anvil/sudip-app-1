0ddc7c74615ec282af172b99d103568cede5e8ec
sudipbh@gmail.com <sudipbh@gmail.com>
2025-02-08 15:30:53 +0000

    Created LICENSE.txt; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -3,8 +3,14 @@ from anvil import *
 
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    def __init__(self, **properties):
+        self.item = {
+            'movie_name': 'Back to the Future',
+            'year': 1985,
+            'director': 'Robert Zemeckis',
+            'summary': 'Doc Brown invents a nuclear powered time machine, which Marty McFly '
+                       'then uses to nearly erase himself from existence.'
+        }
+        
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
